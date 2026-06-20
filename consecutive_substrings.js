9d16ee90039e2d15eefa6e9c87efd69dc4e91e75
jhjulialee <jl4434@nyu.edu>
2026-06-20 16:49:41 -0400

    Lab: DOM Testing - User Behavior Simulation

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = []
+
+  for (let start = 0; start < string.length; start++) {
+    for (let end = start + 1; end <= string.length; end++) {
+      result.push(string.slice(start, end))
+    }
+  }
+
+  return result
 }
 
 if (require.main === module) {
